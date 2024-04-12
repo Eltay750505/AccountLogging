948f40e0e51c518156c93691817ad8781fd57b4c
Roman <eltay.gasimov.jazzteam@gmail.com>
2024-04-12 12:03:10 +0300

    clean up

--- src/main/java/org/gasimov/Main.java
+++ src/main/java/org/gasimov/Main.java
@@ -3,6 +3,7 @@ package org.gasimov;
 
 import org.apache.log4j.Logger;
 import org.gasimov.model.Account;
+import org.gasimov.service.AccountService;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -13,8 +14,10 @@ import java.util.Random;
 public class Main {
 
     private static final Logger logger = Logger.getLogger(Main.class);
+    private static final AccountService accountService = new AccountService();
 
     public static void main(String[] args) {
+        logger.info("Started transfer.");
         int accountsCount = 4;
         BigDecimal baseMoneyValue = BigDecimal.valueOf(10000);
         List<Account> accounts = new ArrayList<>();
@@ -26,7 +29,8 @@ public class Main {
             Long randomId = (long) (random.nextInt(upperBound - lowerBound + 1) + lowerBound);
 
             accounts.add(new Account(randomId, baseMoneyValue));
-            logger.debug("account with id - " + randomId + " created");
         }
+        accountService.transferMoney(accounts.get(0), accounts.get(1), BigDecimal.valueOf(10000));
+        logger.info("Transfer ends.\n");
     }
 }
--- src/main/java/org/gasimov/service/AccountService.java
+++ src/main/java/org/gasimov/service/AccountService.java
@@ -10,8 +10,15 @@ public class AccountService {
     private static final Logger logger = Logger.getLogger(Main.class);
 
     public void transferMoney(Account from, Account to, BigDecimal amount) {
-        from.setMoney(from.getMoney().subtract(amount));
-        to.setMoney(to.getMoney().add(amount));
-        logger.info("Transfer money from account with id - " + from.getId() + " to " + to.getId());
+        BigDecimal subtract = from.getMoney().subtract(amount);
+        if (subtract.compareTo(BigDecimal.ZERO) >= 0) {
+            from.setMoney(subtract);
+            to.setMoney(to.getMoney().add(amount));
+            logger.info("Transfer money from account with id - " + from.getId() + " to " + to.getId());
+            logger.info("Account " + from.getId() + " current amount is - " + from.getMoney());
+            logger.info("Account " + to.getId() + " current amount is - " + to.getMoney());
+        } else {
+            logger.error("Account " + from.getId() + " don't have enough money");
+        }
     }
 }
